9a0fbc701e86c8e462b70464ec59cd4fe5d9ce37
thathcmus <thathcmus@gmail.com>
2023-05-25 18:32:45 +0700

    add convert toool

--- SoundValue/main.c
+++ SoundValue/main.c
@@ -3,30 +3,30 @@
 int main() {
 
     FILE *wavFile = fopen("recording.wav","rb");
-    FILE *dac = fopen("dac_8bit.txt","w");
+    FILE *soundVal = fopen("soundVal_8bit.txt","w");
     fseek(wavFile,20,SEEK_SET);
     int data;
     fread(&data,2,1,wavFile);
-    fprintf("Audio Format: %d\n",data);
+    fprintf(soundVal, "Audio Format: %d\n",data);
     data = 0;
     fread(&data,2,1,wavFile);
-    fprintf("Number Of Channels: %d\n",data);
+    fprintf(soundVal, "Number Of Channels: %d\n",data);
     data = 0;
     fread(&data,4,1,wavFile);
-    fprintf("Sample Rate: %d\n",data);
+    fprintf(soundVal, "Sample Rate: %d\n",data);
     data = 0;
     fread(&data,4,1,wavFile);
-    fprintf("Byte Rate: %d\n",data);
+    fprintf(soundVal, "Byte Rate: %d\n",data);
     data = 0;
     fread(&data,2,1,wavFile);
-    fprintf("Block Align: %d\n",data);
+    fprintf(soundVal, "Block Align: %d\n",data);
     data = 0;
     fread(&data,2,1,wavFile);
-    fprintf("Bits Per Sample: %d\n",data);
+    fprintf(soundVal, "Bits Per Sample: %d\n",data);
     fseek(wavFile,40,SEEK_SET);
     data = 0;
     fread(&data,4,1,wavFile);
-    fprintf("Size: %d\n",data);
+    fprintf(soundVal, "Size: %d\n\n",data);
 
     // Rest code only works  for unsigned 8-bit mono channel wav files.
 
@@ -34,10 +34,10 @@ int main() {
     for(int i = 0 ; i < data ; i++) {
         uint8_t value;
         fread(&value,1,1,wavFile);
-        fprintf(dac,"%d,",value);
+        fprintf(soundVal,"%d,",value);
         x++;
         if(x == 20) {
-            fprintf(dac,"\n");
+            fprintf(soundVal,"\n");
             x=0;
         }
     }
@@ -45,6 +45,6 @@ int main() {
 
 
     fclose(wavFile);
-    fclose(dac);
+    fclose(soundVal);
 
 }
